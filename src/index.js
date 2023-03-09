433a947a91654491220cd6a1de369d37880154e9
YLinDev <adikit1@icloud.com>
2023-03-09 16:47:49 -0500

    setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Example from "./scripts/example";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // console.log("Hello world!")
+    const main = document.getElementById("main")
+    new Example(main); 
+})
