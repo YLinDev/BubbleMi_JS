678129fe23487a90c6afc235c6fb36edf662d87a
Yong <yong.lin13@gmail>
2023-03-09 17:11:31 -0500

    add capy

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,7 +1,7 @@
 class Example {
     constructor(ele) {
         this.ele = ele
-        this.ele.innerHTML = "<h1>It's ALIVEEEE!!!</h1>"
+        this.ele.innerHTML = "<h1>It's Capy ALIVEEEE!!!</h1>"
         this.handleClick = this.handleClick.bind(this);
         this.ele.addEventListener("click", this.handleClick)
     }
