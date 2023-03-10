c952a39868fa93d9b92680f0bdf8f78972a7e1a3
Yong <yong.lin13@gmail>
2023-03-10 16:28:24 -0500

    finish bubble logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import Example from "./scripts/example";
+import {bubbleUp, coinFetch} from "./scripts/data";
+// import Example from "./scripts/example";
 
 document.addEventListener("DOMContentLoaded", () => {
     // console.log("Hello world!")
-    const main = document.getElementById("main")
-    new Example(main); 
+    // const main = document.getElementById("main")
+    // new Example(main); 
+
 })
+
+async function call(){
+    // const json = await coinFetch();
+    const coinInfo = await bubbleUp(); 
+    // console.log(coinInfo);
+    // console.log(typeof coinInfo);
+}
+
+call(); 
